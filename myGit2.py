29c63ddb7168cd3a9566712a65ab50e77f870be5
Vikas Kalra <vkvikaskalra@gmail.com>
2020-07-09 14:54:38 +0530

    Commit1: 9 July 20_2:52 PM

---
+++ myGit2.py
@@ -0,0 +1,7 @@
+"Git: Tool for Version Control"
+"""Through Git we save different versions of our Projects. Whenever we need any
+version ie we want to remove the latest changes from the program, we can do through
+git. """
+
+print("CETPA")
+print("Hello")
